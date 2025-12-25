97233ee4f2c983bccd71b441d27983468ce78a0d
nolimitking <l@nolimitking.com>
2025-12-25 15:54:30 -0500

    v

--- frontend/src/assets/BillionaireBeard.js
+++
@@ -1,6 +0,0 @@
-import bag from "../assets/Kit.png";
-import billionaire_beard_conditioner from "../assets/billionaire_beard_conditioner.png";
-import billionaire_beard_straightener_brush from "../assets/billionaire_beard_straightener_brush.png";
-import billionaire_beard_oil from "../assets/billionaire_Beard_Oil.png";
-import billionaire_beard_wash from "../assets/billionaire_Beard_Wash.png";
-import billionaire_beard_balm from "../assets/billionaire_Beard_Wash2.png";
