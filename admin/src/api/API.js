b389c82050e9ba0b882367e2a76c07d58b6fa2c4
nolimitking <l@nolimitking.com>
2025-12-25 14:27:16 -0500

    Update API.js

--- admin/src/api/API.js
+++ admin/src/api/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: https://no-limit-king-backend.onrender.com,
+  baseURL: "https://no-limit-king-backend.onrender.com",
 });
 
 export default API;
