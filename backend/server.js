87bf33ad83a4cac765cb4267efbeba06d48f836e
nolimitking <l@nolimitking.com>
2025-12-26 17:46:33 -0500

    v

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,14 @@
-import express from "express";
-import cors from "cors";
-import "dotenv/config";
-import connectDB from "./config/db.js";
-import productRoutes from "./Routes/productRoutes.js";
-import authRoutes from "./Routes/authRoutes.js";
-import cartRoutes from "./Routes/cartRoutes.js";
-import checkoutRoutes from "./Routes/checkoutRoutes.js";
-import orderRoutes from "./Routes/orderRoutes.js";
-
-const app = express();
-
-import stripeWebhook from "./controllers/payment/stripeWebhook.js";
-
-app.post(
-  "/api/payment/webhook",
-  express.raw({ type: "application/json" }),
-  stripeWebhook
-);
-
 app.use(express.json());
 app.use(cors());
 connectDB();
 
-const PORT = process.env.PORT || 3000;
+// Redirect non-www to www
+app.use((req, res, next) => {
+  if (req.hostname === "nolimitking.com") {
+    return res.redirect(301, "https://www.nolimitking.com" + req.originalUrl);
+  }
+  next();
+});
 
 // API
 app.use("/api/cart", cartRoutes);
@@ -34,5 +20,3 @@ app.use("/api/order", orderRoutes);
 app.get("/", (req, res) => {
   res.send("Backend is running");
 });
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
